6b55ccc95f36945f6c8ff437c4be87d702dd859e
adityarawat <aditya.rawat@nagarro.com>
2022-10-13 22:42:44 +0530

    2 apis for user and project details

---
+++ src/test/java/apitest/GetUserDetails.java
@@ -0,0 +1,36 @@
+package apitest;
+
+import baseclass.ApiBase;
+import com.relevantcodes.extentreports.LogStatus;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetUserDetails extends ApiBase {
+
+	@Test(priority = 4)
+	public void getTestTemplates() {
+		extentTest = extent.startTest("Fetch current user details");
+		Config config = ConfigFactory.load("Config.properties");
+		RequestSpecification httpRequest = RestAssured.given();
+		Response res = httpRequest.header("Content-Type","application/json")
+				.header("webmate.user",config.getString("webMateUser"))
+				.header("webmate.api-token",config.getString("apiToken"))
+				.when()
+				.get(config.getString("getUserDetails"));
+		extentTest.log(LogStatus.PASS,"API Request created ");
+		String statusLine=res.getStatusLine();
+		Assert.assertEquals(statusLine,"HTTP/1.1 200 OK");
+
+		JsonPath jsonPathEvaluator = res.jsonPath();
+		Assert.assertEquals(jsonPathEvaluator.get("name"),"Aditya Rawat");
+		Assert.assertEquals(jsonPathEvaluator.get("email"),"aditya.rawat@nagarro.com");
+		Assert.assertEquals(jsonPathEvaluator.get("accountState"),"active");
+		logger.info("Valid GetBooking Test Completed");
+	}
+}
